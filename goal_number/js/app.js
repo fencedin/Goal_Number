e2d742c3b9f37d0a32b7cb385467100635766a36
Vincent Rotter <vincent.rotter@gmail.com>
2014-05-06 10:14:33 -0700

    add bootstrap and update model on ember

--- goal_number/js/app.js
+++ goal_number/js/app.js
@@ -1,7 +1,7 @@
 App = Ember.Application.create({
   LOG_TRANSITIONS: true
 });
-App.ApplicationAdapter = DS.FixtureAdapter.extend();
+// App.ApplicationAdapter = DS.FixtureAdapter.extend();
 
 // ------ROUTER-----------------------
 App.Router.map(function() {
@@ -17,28 +17,64 @@ App.GameRoute = Ember.Route.extend({
     this.render({ outlet: 'main' });
   },
   model: function() {
-    return this.store.find('game');
+    //console.log(newGame.get('fullName'));
+    return newGame;
   }
 });
 
 
 // --------CONTROLLERS---------------------
-App.GameController = Ember.ArrayController.extend({
+App.GameController = Ember.ObjectController.extend({
   actions: {
-    add1: function(){
-      var store = this.get('store');
-      var num = 1
-
-      var game = this.get('model');
-      console.log(game)
-      debugger;
-      var currentnum =  game['number']
-
-
-      console.log(currentnum);
-
-
+    add: function(){
+      var num = this.get('number');
+      num += Math.floor((Math.random() * 9) + 1);
+      var current = this.get('model');
+      current.set('number', num)
+      current.save
+      console.log(this.get('number'));
+    },
+    sub: function(){
+      var num = this.get('number');
+      num -= Math.floor((Math.random() * 9) + 1);
+      var current = this.get('model');
+      current.set('number', num)
+      current.save
+      console.log(this.get('number'));
+    },
+    tim: function(){
+      var num = this.get('number');
+      num *= Math.floor((Math.random() * 9) + 1);
+      var current = this.get('model');
+      current.set('number', num)
+      current.save
+      console.log(this.get('number'));
+    },
+    div: function(){
+      var num = this.get('number');
+      num /= Math.floor((Math.random() * 9) + 1);
+      num = Math.floor(num);
+      var current = this.get('model');
+      current.set('number', num)
+      current.save
+      console.log(this.get('number'));
     }
   }
 });
 
+App.Game = Ember.Object.extend({
+  number: null,
+  goal: null,
+  rand: null,
+  fullName: function() {
+    return this.get('number') + ' ' + this.get('goal');
+  }.property('number', 'goal'),
+  randNum: function() {
+    return Math.floor((Math.random() * 9) + 1);
+  }.property('rand')
+});
+
+var newGame = App.Game.create({
+  number: 1,
+  goal:  1000
+});
