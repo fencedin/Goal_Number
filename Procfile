5c07249df6ea845b23d48b8444a764370ca08d45
Vincent Rotter <vincent.rotter@gmail.com>
2014-05-06 13:29:32 -0700

    Go Test

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: goal_number
---
+++ server.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+  "net/http"
+  "os"
+)
+
+func main() {
+        panic(http.ListenAndServe(":" + os.Getenv("PORT"),
+          http.FileServer(http.Dir("./goal_number"))))
+}
